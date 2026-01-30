573d62cd8d00bee4e6cabb1423b91540500a70cc
BimanJana995 <bimanj@op.iitg.ac.in>
2026-01-30 10:13:05 +0530

    Update Visulisation.py

--- Visulisation.py
+++ Visulisation.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 def show_chart():
     df = pd.read_csv('finance_data.csv')
-    # Category ke hisab se group karna
     category_data = df.groupby('Category')['Amount'].sum()
     
-    # Pie chart banana
     category_data.plot(kind='pie', autopct='%1.1f%%')
     plt.title('Mera Kharcha (Category-wise)')
     plt.show()
 
 show_chart()
+
