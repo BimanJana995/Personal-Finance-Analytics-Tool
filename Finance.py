742fed802cf96452ffe8b41b1ad42b12f7ac2ac6
BimanJana995 <bimanj@op.iitg.ac.in>
2026-01-30 10:13:51 +0530

    Update Finance.py

--- Finance.py
+++ Finance.py
@@ -5,17 +5,18 @@ def add_expense(date, category, desc, amount):
     data = {'Date': [date], 'Category': [category], 'Description': [desc], 'Amount': [amount]}
     df = pd.DataFrame(data)
     
-    # Agar file pehle se hai toh usme add karo, nahi toh nayi banao
+    
     if not os.path.isfile('finance_data.csv'):
         df.to_csv('finance_data.csv', index=False)
     else:
         df.to_csv('finance_data.csv', mode='a', index=False, header=False)
     print("Kharcha save ho gaya!")
 
-# User se input lena
+
 date = input("Date (DD-MM-YYYY): ")
 cat = input("Category: ")
 desc = input("Description: ")
 amt = float(input("Amount: "))
+
 
 add_expense(date, cat, desc, amt)
